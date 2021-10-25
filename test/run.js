bcbe4d9ddf2cf504ecc68ce5a2d18bdd1be08e84
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2021-10-25 20:53:55 +0200

    Use `fork` instead of `spawn`

--- test/run.js
+++ test/run.js
@@ -1,19 +1,17 @@
 import cp from 'node:child_process';
 
 export default function run(file, args = [], opts = {}) {
-	let mem = [];
-	let child = cp.spawn('node', [
-		'--experimental-loader=node-esm-loader',
-		'--no-warnings',
-		file,
-		...args,
-	], opts);
-	child.stderr.pipe(process.stderr);
-	child.stdout.pipe(process.stdout);
+	const child = cp.fork(file, args, {
+		execArgv: [
+			`--experimental-loader=node-esm-loader`,
+			'--no-warnings',
+		],
+		...opts,
+	})
 	return new Promise((resolve, reject) => {
 		child.on('exit', code => {
 			if (code) {
-				reject(new Error({ message: `Error code ${code}`}));
+				reject(new Error(`Error code ${code}`));
 			} else {
 				resolve(code);
 			}
