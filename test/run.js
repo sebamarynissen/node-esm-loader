085226d69e697b2384026528ecbdc2aa0b09d39f
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2023-12-14 11:08:13 +0100

    Update ci matrix for tests

--- test/run.js
+++ test/run.js
@@ -2,15 +2,9 @@ import cp from 'node:child_process';
 import semver from 'semver';
 
 export default function run(file, args = [], opts = {}) {
-	let flag = (
-		semver.satisfies(process.version, '>=20.6') ?
-		'--import=node-esm-loader/register' :
-		'--experimental-loader=node-esm-loader'
-	);
 	const child = cp.fork(file, args, {
 		execArgv: [
-			flag,
-			'--no-warnings',
+			'--import=node-esm-loader/register',
 		],
 		...opts,
 	})
