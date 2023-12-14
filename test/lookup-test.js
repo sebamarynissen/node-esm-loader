0ecc08bdec321b2bb445f085fb5cd2914347ca89
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2023-12-14 12:25:27 +0100

    Refactor tests

--- test/lookup-test.js
+++ test/lookup-test.js
@@ -9,6 +9,10 @@ const __dirname = path.dirname(__filename);
 
 describe('The lookup function', function() {
 
+	function result(basename) {
+		return path.join(__dirname, basename);
+	}
+
 	function find(folder) {
 		return findConfig(path.join(__dirname, folder));
 	}
@@ -16,35 +20,35 @@ describe('The lookup function', function() {
 	it('finds the .loaderrc.js file in the same folder', async function() {
 
 		let file = await find('project');
-		expect(file).to.equal(path.join(__dirname, 'project/.loaderrc.js'));
+		expect(file).to.equal(result('project/.loaderrc.js'));
 
 	});
 
 	it('finds the .loaderrc.js file in the upper folder', async function() {
 
 		let file = await find('upper/sub');
-		expect(file).to.equal(path.join(__dirname, 'upper/loader.config.js'));
+		expect(file).to.equal(result('upper/loader.config.js'));
 
 	});
 
 	it('supports .mjs files', async function() {
 
 		let file = await find('mjs');
-		expect(file).to.equal(path.join(__dirname, 'mjs/.loaderrc.mjs'));
+		expect(file).to.equal(result('mjs/.loaderrc.mjs'));
 
 	});
 
 	it('supports *.config.js files', async function() {
 
 		let file = await find('dot-config');
-		expect(file).to.equal(path.join(__dirname, 'dot-config/loader.config.js'));
+		expect(file).to.equal(result('dot-config/loader.config.js'));
 
 	});
 
 	it('supports .config folders', async function() {
 
 		let file = await find('config-folder');
-		expect(file).to.equal(path.join(__dirname, 'config-folder/.config/loaderrc.js'));
+		expect(file).to.equal(result('config-folder/.config/loaderrc.js'));
 
 	});
 
